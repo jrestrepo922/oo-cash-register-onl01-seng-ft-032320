61ed5ca68f86d1a11eb0dbe19366137d3041ffff
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-04 12:41:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,48 @@
+require "pry"
+class CashRegister
+  
+  attr_accessor :total, :discount, :price, :items
+  
+  
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end 
+  
+  def add_item(item, price, quantity = 1)
+    @price = price
+    @total = @total + price * quantity
+    
+    count = 0 
+    
+    while count < quantity
+      @items << item
+      count += 1
+    end 
+    
+    
+  end
+  
+  def apply_discount
+    if @discount >  0 
+      @to_take_off = (price * discount)/100 
+      @total -= @to_take_off
+      "After the discount, the total comes to $#{total}."
+    else 
+      "There is no discount to apply."
+    end 
+  end 
+  
+  def void_last_transaction
+    @items.clear
+    if items.length == 0 
+      @total = 0
+    else 
+      @total -= @price
+    end 
+  end 
+  
+end 
 
